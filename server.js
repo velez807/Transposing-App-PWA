5be74a2a203bb74fb6ea635f4fd2a6b46eb78568
Velez807 <sebasdelvelez@gmail.com>
2023-06-21 15:48:39 -0500

    Correcciones pwa

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('/'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(port, () => {
